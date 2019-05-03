0033702c14b53b70cc4749807d019246f2f30b2e
Boba-Syu <1914808114@qq.com>
2019-05-03 16:45:28 +0800

    添加了快排

---
+++ QuickSort.java
@@ -0,0 +1,41 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        int a[] = new int[]{2,1,5,7,9,0,6,4,3,8};
+        for(int i = 0; i < 10; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+
+        sort(a, 0, 9);
+        for(int i = 0; i < 10; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void sort(int[] a, int lo, int hi) {
+        if(hi <= lo) return;
+        int j =  partition(a, lo, hi);
+        sort(a, lo, j-1);
+        sort(a, j+1, hi);
+    }    
+    
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        int v = a[lo];
+        while(true) {
+            while(a[++i] < v) 
+                if(i == hi) break;
+            while(a[--j] > v) 
+                if(j == lo) break;
+            if(i >= j) break;
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+        int tmp = a[lo];
+        a[lo] = a[j];
+        a[j] = tmp;
+        return j;
+    }
+}
