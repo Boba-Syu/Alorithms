06ec0951428c870c4ccf5cd7b68da8cfbabce36d
Boba-Syu <1914808114@qq.com>
2019-05-26 19:13:13 +0800

    红黑树增删改查

--- Vol.3/RedBlackBST.java
+++ Vol.3/RedBlackBST.java
@@ -30,7 +30,7 @@ public class RedBlackBST {
         return x.N;
     }
 
-    private isRed() {
+    private boolean isRed(Node x) {
         if(x == null)
             return BLACK;
         return x.color == RED;
@@ -54,5 +54,64 @@ public class RedBlackBST {
         h.color = RED;
         x.H = h.H;
         h.H = 1 + size(h.left) + size(h.right);
+    }
+
+    /**
+     * 将一个结点和两个红色子节点变色, 此时该以结点为根结点的数高度+1
+     */
+    public void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    public void put(int Key, int value) { // 修改键为key的结点的值
+        root = put(root, key, value);
+        root.color = BLACK;
+    }
+
+    public Node put(Node h, int key, int value) { // 在指定树中修改键为key的结点的值
+        if(h == null) 
+            return new Node(key, val, 1, RED);
+
+        if(key < h.key)
+            h.left = put(h.left, key, value);
+        else if(key > h.key)
+            h.right = put(h.right, key, value);
+        else
+            h.val = value;
+        
+        if(isRed(h.right) && !isRed(h.left)) // 调整红黑树的结构
+            h = rotateLeft(h);  
+        if(isRed(h.left) && !isRed(h.left.left))
+            h = rotateRight(h);
+        if(isRed(h.left) && !isRed(h.right))
+            flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+        return h;
+    }
+    
+    public void deleteMin() { // 删除最小的结点
+        root = deleteMin(root);
+    }
+
+    public Node deleteMin(Node h) { // 删除指定树中最小的结点
+        if (h == null)
+            return null;
+        if(h.left != null && h.left.left != null) 
+            h = deleteMin(h.left);
+        if(h.left != null && h.left.left == null) {
+            if(isRed(h.left))
+                h.left = null;
+            else if(!isRed(h.left) && isRed(h.right)) {
+                h = rotateLeft(h);
+                h.left = null;
+                h.color = BLACK;
+            } else if (!isRed(h.left) && !isRed(h.right)) {
+                //暂时不会, 以后再改
+            }
+        }
+        return h;
     }
 }
