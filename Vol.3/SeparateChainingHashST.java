2e6b0fa627b587bfd51b02b3b7dae00b6141ea77
Boba-Syu <1914808114@qq.com>
2019-06-05 20:22:39 +0800

    基于拉链法的散列表

---
+++ Vol.3/SeparateChainingHashST.java
@@ -0,0 +1,67 @@
+
+/**
+ * 基于拉链法的散列表
+ */
+public class SeparateChainingHash<Key, Value> {
+    private int N; // 键值对总数
+    private int M; // 散列表大小
+    private SequentialSearchST<Key, Value>[] st;
+    public SeparateChainingHash(){
+        SeparateChainingHash(997);
+    }
+
+    public SeparateChainingHash(int m) {
+        this.M = m;
+        st = new SequentialSearchST<Key, Value>[m];
+        for(int i = 0; i < m; i++) {
+            st[i] = new SequentialSearchST<Key, Value>();
+        }
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public Value get(Key key) {
+        return st[hash(key)].get(key);
+    }
+
+    public void put(Key key, Value value) {
+        st[hash(key)].put(key, value);
+    }
+
+    private class SequentialSearchST<Key, Value> {
+        private Key key;
+        private Value value;
+        private SequentialSearchST<Key, Value> next;
+
+        public SequentialSearchST() {
+
+        }
+
+        public SequentialSearchST(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public void put(Key key, Value value) {
+            if (this.key.equals(key))
+                this.value = value;
+            else if (this.next == null) {
+                this.next = new SequentialSearchST<Key, Value>(key, value);
+            } else
+                this.next.put(key, value);
+        }
+
+        public Value get(Key key) {
+            if (this.key.equals(key))
+                return this.value;
+            else {
+                if (this.next == null)
+                    return null;
+                else
+                    return this.next.get(key);
+            }
+        }
+    }
+}
