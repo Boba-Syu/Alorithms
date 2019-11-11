ca3188243d857f004bfe5b65c070c58ee5d8f361
Boba-Syu <bobasyu@163.com>
2019-11-11 14:09:45 +0800

    二叉查找树

---
+++ src/Vol_3/BST.java
@@ -0,0 +1,194 @@
+package Vol_3;
+
+/**
+ * 二叉查找树
+ *
+ * @param <Key>
+ * @param <Value>
+ */
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left;
+        private Node right;
+        private int N;
+
+        public Node(Key key, Value value, int n) {
+            this.key = key;
+            this.value = value;
+            N = n;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+    }
+
+    public Key min() {
+        return min(root).key;
+    }
+
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
+    }
+
+    public Key select(int k) {
+        return select(root, k).key;
+    }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    public Node deleteMin() {
+        return deleteMin(root);
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.N;
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        }
+        return x.value;
+    }
+
+    private Node put(Node x, Key key, Value value) {
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+
+    private Node min(Node x) {
+        if (x == null) {
+            return null;
+        }
+        return min(x.left);
+    }
+
+    /**
+     * 大于等于key的最小键
+     *
+     * @param x
+     * @param key
+     * @return
+     */
+    private Node floor(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return floor(x.left, key);
+        } else if (cmp == 0) {
+            return x;
+        }
+        Node t = floor(x.right, key);
+        if (t != null) {
+            return t;
+        }
+        return x;
+    }
+
+    private Node select(Node x, int k) {
+        if (x == null) {
+            return null;
+        }
+        int t = size(x.left);
+        if (t > k) {
+            return select(x.left, k);
+        } else if (t < k) {
+            return select(x.right, k - t - 1);
+        }
+        return x;
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return rank(key, x.left);
+        } else if (cmp > 0) {
+            return rank(key, x.right) + size(x.left) + 1;
+        }
+        return size(x.left);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = delete(x.left, key);
+        } else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            if (x.right == null) {
+                return x.left;
+            }
+            if (x.left == null) {
+                return x.right;
+            }
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+}
