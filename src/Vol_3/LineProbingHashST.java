5220fdef08fd094fe9953702cba114aa590561bb
Boba-Syu <bobasyu@163.com>
2019-11-15 17:32:16 +0800

    基于线性探测的符号表的get方法

--- src/Vol_3/LineProbingHashST.java
+++ src/Vol_3/LineProbingHashST.java
@@ -50,6 +50,16 @@ public class LineProbingHashST<Key, Value> {
         N++;
     }
 
+    public Value get(Key key) {
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % M) {
+            if (keys[i].equals(key)) {
+                return values[i];
+            }
+        }
+        return null;
+    }
+
     public void delete(Key key) {
         if (!contains(key)) {
             return;
@@ -73,12 +83,7 @@ public class LineProbingHashST<Key, Value> {
     }
 
     public boolean contains(Key key) {
-        for (Key k : keys) {
-            if (k.equals(key)) {
-                return true;
-            }
-        }
-        return false;
+        return get(key) != null;
     }
 
     /**
