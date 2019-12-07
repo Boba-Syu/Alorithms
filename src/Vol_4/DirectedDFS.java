20a9228d204e5a7720845781b4c24ee30088600c
Boba-Syu <bobasyu@163.com>
2019-12-07 11:31:30 +0800

    有向图的DFS

--- src/Vol_4/DirectedDFS.java
+++ src/Vol_4/DirectedDFS.java
@@ -1,13 +1,20 @@
 package Vol_4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 有向图的DFS
  */
 public class DirectedDFS {
     private boolean[] marked;
+    private int s;
+    private int[] edgeTo;
 
     public DirectedDFS(Digraph g, int s) {
         marked = new boolean[g.getV()];
+        edgeTo = new int[g.getV()];
+        this.s = s;
         dfs(g, s);
     }
 
@@ -15,6 +22,7 @@ public class DirectedDFS {
         marked[v] = true;
         for (int w : g.adj(v)) {
             if (!marked[w]) {
+                edgeTo[v] = w;
                 dfs(g, w);
             }
         }
@@ -28,5 +36,23 @@ public class DirectedDFS {
      */
     public boolean hasPathTo(int v) {
         return this.marked[v];
+    }
+
+    /**
+     * 获取起点s到顶点v的路径
+     *
+     * @param v
+     * @return
+     */
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        List<Integer> path = new ArrayList<>();
+        for (int i = v; i != s; i = edgeTo[i]) {
+            path.add(i);
+        }
+        path.add(s);
+        return path;
     }
 }
